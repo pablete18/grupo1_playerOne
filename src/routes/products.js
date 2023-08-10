813ab366c4d352732787e55187243ae873dc15d8
pablete18 <pabloealcaraz@gmail.com>
2023-08-10 00:23:02 -0300

    fix: rutas

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { detail, carrito,editProducts } = require('../controllers/productsController');
+const {detail, carrito, editProducts} = require('../controllers/productsController');
 
 const router = express.Router();
 
