b5a96375d81efe831f0f3800d1958b55302f8c44
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-08-23 17:05:09 -0300

    agregando admin

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,8 @@
 module.exports = {
     index : (req, res) => {
         return res.render('index');
+    },
+    admin : (req,res) => {
+        return res.render('admin')
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { index } = require('../controllers/indexController');
+const { index, admin } = require('../controllers/indexController');
 const router = express.Router();
 
 
 
 /* / */
 router.get('/', index);
+router.get('/admin', admin)
 
 module.exports = router;
