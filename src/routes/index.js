86ee4e8274e88f6ad68da5511d67af6a477f1fa8
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-08-23 21:31:40 -0300

    modificando rama

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { index, admin } = require('../controllers/indexController');
+const { index } = require('../controllers/indexController');
 const router = express.Router();
 
 
 
 /* / */
 router.get('/', index);
-router.get('/admin', admin)
 
 module.exports = router;
