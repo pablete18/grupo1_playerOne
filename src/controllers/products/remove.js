9f616f91fc8cfe7178db7c8d94a1c809f5f8141d
pablete18 <pabloealcaraz@gmail.com>
2023-08-24 21:02:50 -0300

    feat: controller remove

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -1,5 +1,17 @@
-const { readJSON, writeJSON } = require("../../data/products.json")
+const fs = require('fs');
+const path = require ('path');
+const productsFilePath = path.join(__dirname,'../../data/products.json')
+const products = JSON.parse (fs.readFileSync(productsFilePath, 'utf-8'))
 
-module.exports = {
 
-} 
+module.exports =(req,res)=> {
+   
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		
+		
+    const productModify = products.filter(product => product.id !== +req.params.id)
+    fs.writeFileSync(path.join(__dirname,'../data/products.json'),JSON.stringify(productModify,null,3))
+
+    return res.redirect('/index')
+
+}
--- src/controllers/products/update.js
+++ src/controllers/products/update.js
@@ -1,6 +1,6 @@
 const { unlinkSync, existsSync } = require("fs");
 const { readJSON, writeJSON } = require("../../data/products.json");
 
-/* module.exports= {
+module.exports= {
 
-} */
+} 
