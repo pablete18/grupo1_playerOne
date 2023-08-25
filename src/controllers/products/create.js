2fa75b1e522e3a76ad823589840eee9dd4ce9e96
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-08-25 18:44:37 -0300

    modificando image

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -5,5 +5,22 @@ const productsFilePath = path.join(__dirname, '../../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
  module.exports = (req, res) => {
-    return res.render('admin')
+    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+    const {name,price, discount,category,description} = req.body;
+    const product = {
+        id:products[products.length-1].id+1,
+        name: name?.trim(),
+        price: +price,
+        discount: +discount,
+        category,
+        description: description?.trim(),
+        image: null
+
+    }
+    products.push(product)
+
+    fs.writeFileSync(path.join(__dirname,'../../data/products.json'),JSON.stringify(products,null,3))
+
+    res.redirect('/admin')
+    
 }
