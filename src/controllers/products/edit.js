48fb0116220f0479d868226d6ba0731b07cff85f
pablete18 <pabloealcaraz@gmail.com>
2023-08-25 17:50:02 -0300

    add: fix productEdit

--- src/controllers/products/edit.js
+++ src/controllers/products/edit.js
@@ -5,16 +5,12 @@ const products = JSON.parse (fs.readFileSync(productsFilePath, 'utf-8'))
 
 
 module.exports =  (req, res) => {
-
-    const products = readJSON("products.json");
-    const users = readJSON("brands.json");
-
+    const products = JSON.parse (fs.readFileSync(productsFilePath, 'utf-8'));
     const id = req.params.id;
-    const product = products.find((product) => product.id === id);
+    const product = products.find((product) => product.id ===+id);
 
     return res.render("productEdit", {
-        ...product
-    /*     brands: brands.sort((a, b) =>
-        a.name > b.name ? 1 : a.name < b.name ? -1 : 0 */
+        product
+    
     })
   }
