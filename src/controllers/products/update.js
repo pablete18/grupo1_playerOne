f6e74d4d526ad82a8b2b48fbcdd5b1b512d34447
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-09-02 00:18:29 -0300

    arreglos de edit y delete

--- src/controllers/products/update.js
+++ src/controllers/products/update.js
@@ -1,25 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const productsFilePath = path.join(__dirname, '../../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-module.exports= (req, res) => {
-	const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-		
-		const {name,price,estado, category,description} = req.body;
-		const productModify = products.map((product=>{			
-			if(product.id=== +req.params.id){
-				product.name= name.trim()
-				product.price= +price
-				product.estado= estado
-				product.category = category
-				product.description= description.trim()
+module.exports = (req, res) => {
+  const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-			}
-			return product
-		}))
-		fs.writeFileSync(path.join(__dirname,'../../data/products.json'),JSON.stringify(productModify,null,3),'utf-8')
-		
-		return res.redirect('/index')
+  const { name, price, estado, category, description } = req.body;
+  const productId = +req.params.id;
 
-} 
+  const productModify = products.map((product) => {
+    if (product.id === productId) {
+     
+      if (typeof name === 'string') {
+        product.name = name.trim();
+      }
+      product.price = +price;
+      product.estado = estado;
+      product.category = category;
+     
+      if (typeof description === 'string') {
+        product.description = description.trim();
+      }
+    }
+    return product;
+  });
+
+  fs.writeFileSync(
+    path.join(__dirname, '../../data/products.json'),
+    JSON.stringify(productModify, null, 3),
+    'utf-8'
+  );
+
+  return res.redirect('/admin');
+};
