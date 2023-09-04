8b1d2188ca3169254f0d13a35175073587eb9fc0
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-09-04 18:45:14 -0300

    empezando detalles del sprint5

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -4,23 +4,23 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
- module.exports = (req, res) => {
-    const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-    const {name,price, discount,category,description} = req.body;
+module.exports = (req, res) => {
+    const { name, price, discount, description } = req.body;
+    
     const product = {
-        id:products[products.length-1].id+1,
+        id: products.length + 1, 
         name: name?.trim(),
-        price: +price,
-        discount: +discount,
-        category,
+        price: parseFloat(price), 
+        discount: parseFloat(discount), 
         description: description?.trim(),
         image: null
+    };
 
-    }
-    products.push(product)
+   
+    products.push(product);
 
-    fs.writeFileSync(path.join(__dirname,'../../data/products.json'),JSON.stringify(products,null,3))
-
-    res.redirect('/admin')
     
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 3));
+
+    res.redirect('/admin');
 }
--- src/controllers/products/edit.js
+++ src/controllers/products/edit.js
@@ -1,45 +1,16 @@
 const fs = require('fs');
-const path = require('path');
-const productsFilePath = path.join(__dirname, '../../data/products.json');
+const path = require ('path');
+const productsFilePath = path.join(__dirname,'../../data/products.json')
+const products = JSON.parse (fs.readFileSync(productsFilePath, 'utf-8'))
 
-module.exports = (req, res) => {
+
+module.exports =  (req, res) => {
+    const products = JSON.parse (fs.readFileSync(productsFilePath, 'utf-8'));
     const id = req.params.id;
-    fs.readFile(productsFilePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error interno del servidor');
-        }
+    const product = products.find((product) => product.id ===+id);
 
-        const products = JSON.parse(data);
-        const productIndex = products.findIndex((product) => product.id === +id);
-
-        if (productIndex === -1) {
-            return res.status(404).send('Producto no encontrado');
-        }
-
-        const updatedProduct = {
-            id: products[productIndex].id,
-            name: req.body.name || products[productIndex].name,
-            estado: req.body.estado || products[productIndex].estado,
-            price: req.body.price || products[productIndex].price,
-            discount: req.body.discount || products[productIndex].discount,
-            description: req.body.description || products[productIndex].description,
-        
-            image: req.file ? req.file.filename : products[productIndex].image,
-        };
-
-        products[productIndex] = updatedProduct;
-
-       
-        fs.writeFile(productsFilePath, JSON.stringify(products, null, 2), (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Error al guardar el producto');
-            }
-
-            return res.redirect('/admin'); 
-        });
-    });
-};
-
-
+    return res.render("productEdit",{
+            product,
+    
+    })
+  }
